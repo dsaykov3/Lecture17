ff5b55aad7a1ce6c4627642cbbe1ea2a4c524ee9
Dimitar Saykov <dimitar.saykov.ext@spotlightsportsgroup.com>
2022-12-15 18:14:06 +0200

    initial

---
+++ src/main/java/com/progress/ContactdbSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.progress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication()
+public class ContactdbSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactdbSpringBootApplication.class, args);
+	}
+
+}
